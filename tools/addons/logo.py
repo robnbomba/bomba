db11351a8fc11d2225153a12a3221fa281dfff80
robnbomba <40227362+robnbomba@users.noreply.github.com>
2020-10-11 15:13:47 +0300

    Add files via upload

---
+++ tools/addons/logo.py
@@ -0,0 +1,13 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+  ____   ____  __  __ ____          
+ |  _ \ / __ \|  \/  |  _ \   /\    
+ | |_) | |  | | \  / | |_) | /  \   
+ |  _ <| |  | | |\/| |  _ < / /\ \  
+ | |_) | |__| | |  | | |_) / ____ \ 
+ |____/ \____/|_|  |_|____/_/    \_\
+                    Created by robn
+{Fore.RESET}""")
